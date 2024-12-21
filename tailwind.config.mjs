5089d62ae639423988952e48bbe304a32a0ce6ea
SumiyaMim <sumiyaislam1093@gmail.com>
2024-12-21 12:52:13 +0600

    change theme

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -8,8 +8,8 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: "var(--background, white)", 
+        foreground: "var(--foreground, black)",
       },
     },
   },
